3202536e0239c98b47c2a99c3b3df5b8fceffc9f
Saran-21 <80886595+Saran-21@users.noreply.github.com>
2021-08-17 15:49:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 prediction_1 = ""
-prediction_2 = ""
 Webcam.set({
     width: 360,
     height: 250,
@@ -17,15 +16,14 @@ function take_snapshot(){
 }
 
 console.log("ml5 version", ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JtC8qB00Q/', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JtC8qB00Q/model.json', modelLoaded);
 function modelLoaded(){
     console.log("model loaded");
 }
 function speak(){
     var synth = window.speechSynthesis;
     speak_data_1 = "the first prediction is " + prediction_1;
-    speak_data_2 = "and the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
 
 }
@@ -40,9 +38,7 @@ function gotResult(error,results){
     else{
         console.log(results);
         document.getElementById("result_emotion_name").innerHTML = results[0].label;
-        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
         prediction_1 = results[0].label;
-        prediction_2 = results[1].label;
         speak();
         if(results[0].label == "Victory")
         {
